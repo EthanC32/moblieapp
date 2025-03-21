ba24c060cecfdae690f72de9a616fe1edc0b2d14
EthanC32 <kachun_822@outlook.com>
2025-03-21 20:23:02 +0800

    Add files via upload

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import data from './data.json';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Home Screen!</Text>
-    </View>
+      <Text style={styles.header}>Todo List (For Loop)</Text>
+      {(() => {
+        const items = [];
+
+        for (let i = 0; i < data.length; i++) {
+          const item = data[i];
+          const date = new Date(item.toLocaleDate)
+          const formattedDate = date.toLocaleDateString()
+          const formattedTime = date.toLocaleTimeString()
+          
+          items.push(
+            <View>
+              <Text>{item.title}</Text>
+              <Text>{item.description}</Text>
+              <Text>By: {item.user} - Comments: {item.commentCount}</Text>
+              <Text>{formattedDate} {formattedTime}</Text>
+            </View>  
+
+          );
+        }
+        return (
+          <View style={styles.container}>
+            <Text style={style.header}>Todo List (For loop)</Text>
+            {items}
+            </View>
+        );
+      })()}
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-  },
-});
-
-export default HomeScreen;
